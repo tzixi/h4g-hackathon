5524ca6a2242c9e22a17d7ceeac9c5db2d15b685
thomasjlalba <e0773507@u.nus.edu>
2023-02-08 17:28:11 +0800

    Changed signin page to look like figma

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,19 +1,56 @@
 import { NextPage } from "next";
-
+import Head from "next/head";
+import Stack from "@mui/material/Stack";
+import Fab from '@mui/material/Fab';
+import { Container } from "@mui/material";
+import styles from '../../styles/Signin.module.css'
+import cx from 'classnames';
+import { ST } from "next/dist/shared/lib/utils";
 
 interface Props {}
 
+
 const SignIn: NextPage = (props): JSX.Element => {
     return (
-    <div className="sign-in-form">
-        <form>
-            <h1>User Login</h1>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <input type="submit" value="Assessor Login" />
-            <input type="submit" value="Coprporate Login" />
-        </form>
-    </div>
+        <>
+            <Head>
+                <title>Login</title>
+            </Head>
+
+            <Container>
+                <div className={cx(styles["sign-in-form"])}>
+                    <form>
+                        <h1>User Login</h1>
+                        {/* <Stack direction="row" spacing={2} marginBottom={2}>
+                            <label>Username:</label>
+                            <input type="text" placeholder="Username" />
+                        </Stack>
+
+                        <Stack direction="row" spacing={2} marginBottom={5}>
+                            <label>Password:</label>
+                            <input type="password" placeholder="Password" />
+                        </Stack> */}
+
+                        <Stack direction="row" spacing={2} marginBottom={5} textAlign="center">
+                            <Stack direction="column" spacing={5} paddingTop={1}>
+                                <label>Username:</label>
+                                <label>Password:</label>
+                            </Stack>
+
+                            <Stack direction="column" spacing={2}>
+                                <input type="text" placeholder="Username" />
+                                <input type="password" placeholder="Password" />
+                            </Stack>
+                        </Stack>
+                        
+                        <Stack direction="row" spacing={4} justifyContent="center">
+                            <Fab aria-label="assessor-login" variant="extended" color="primary" size="medium">Assessor Login</Fab> 
+                            <Fab aria-label="corporate-login" variant="extended" color="secondary" size="medium">Coporate Login</Fab>
+                        </Stack>
+                    </form>
+                </div>
+            </Container>
+        </>
     );
 };
 
