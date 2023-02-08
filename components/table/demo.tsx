1f0565f2b2b9552324e87cf8a516cbd8817828af
tzixi <tanzixi9@gmail.com>
2023-02-08 23:59:28 +0800

    added Assessor page with dummy table

---
+++ components/table/demo.tsx
@@ -0,0 +1,70 @@
+import Button from '@mui/material/Button';
+import { DataGrid, GridColDef, GridApi, GridCellValue } from '@mui/x-data-grid';
+
+const columns: GridColDef[] = [
+  {
+    field: 'action',
+    headerName: 'Assess Website',
+    width: 120,
+    sortable: false,
+    renderCell: (params : any) => {
+      const onClick = (e : any) => {
+        e.stopPropagation(); // don't select this row after clicking
+
+        const api: GridApi = params.api;
+        const thisRow: Record<string, any> = {};
+
+        api
+          .getAllColumns()
+          .filter((c : any) => c.field !== '__check__' && !!c)
+          .forEach(
+            (c : any) => (thisRow[c.field] = params.getValue(params.id, c.field)),
+          );
+
+        return alert(JSON.stringify(thisRow, null, 4));
+      };
+
+      return <Button onClick={onClick}>Click</Button>;
+    },
+  },
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'Website', width: 300 },
+  { field: 'lastName', headerName: 'Company', width: 300 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params : any) =>
+      `${params.getValue(params.id, 'firstName') || ''} ${
+        params.getValue(params.id, 'lastName') || ''
+      }`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function DataGridDemo() {
+  return (
+    <div style={{ height: 400, width: '100%', backgroundColor: "white"}}>
+      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+    </div>
+  );
+}
---
+++ components/table/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DataGridDemo from './demo';
+import Demo from './demo'
+
+ReactDOM.render(<Demo />, document.querySelector('#root'));
+    
