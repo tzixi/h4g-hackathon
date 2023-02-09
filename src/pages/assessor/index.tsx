a8fb9d79bab4cce80a3b9e3b0795f6c4a06e364b
firwer <firwer@gmail.com>
2023-02-09 22:37:21 +0800

    fix: Assessor page scrolling issue and changes to UI design

--- src/pages/assessor/index.tsx
+++ src/pages/assessor/index.tsx
@@ -3,38 +3,52 @@ import Image from "next/image";
 import Head from "next/head";
 import DataGridDemo from "../../../components/table/demo";
 import styles from "./assessor.module.css";
+import { fontWeight } from "@mui/system";
+import { Fab } from "@mui/material";
 
 function Home() {
   return (
-    <>
+    <div style={{ padding: "20px" }}>
       <Head>
         <title>Assessor Page</title>
       </Head>
-      <div style={{ overflow: "hidden", objectPosition: "bottom", backgroundColor: "#f9f9f9" }}>
+      <div
+        style={{
+          overflow: "hidden",
+          objectPosition: "bottom",
+          backgroundColor: "#f9f9f9",
+        }}
+      >
         <Image src="/InclusionLabLogo.png" alt="me" width="500" height="118" />
       </div>
-      <div style={{backgroundColor: "#f9f9f9"}}>    
+      <div style={{ backgroundColor: "#f9f9f9" }}>
         <div className={styles.description}>
           <div>
-            <p>Assessor Name: Tan Ah Gow</p>
+            <p style={{ fontSize: "20px", fontWeight: "700" }}>
+              ASSESSOR DASHBOARD - TAN AH GOW
+            </p>
           </div>
           <div></div>
-            <div className={styles.right}>
-              <Image src="/user.png" alt="profile" width="50" height="50"/>
-              <p>Login</p>
-              <p>My Profile</p>
-            </div>
+          <div className={styles.right}>
+            <Image src="/user.png" alt="profile" width="50" height="50" />
+            <Fab sx={{ margin: "0 20px" }} variant="extended">
+              MY PROFILE
+            </Fab>
+            <Fab variant="extended">LOG OUT</Fab>
+          </div>
         </div>
       </div>
-      <div style={{backgroundColor: "#f9f9f9"}}>
-      <div className={styles.left}>
-        <p>Websites available for assessment:</p>
-      </div>
+      <div style={{ backgroundColor: "#f9f9f9" }}>
+        <div className={styles.left}>
+          <p style={{ fontSize: "20px", fontWeight: "700" }}>
+            WEBSITE(S) AVAILABLE FOR ASSESSMENT
+          </p>
+        </div>
       </div>
       <main className={styles.main}>
         <DataGridDemo />
       </main>
-    </>
+    </div>
   );
 }
 
