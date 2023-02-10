bfc9253fa67480a7a0515eafbb8498444325f030
tzixi <tanzixi9@gmail.com>
2023-02-11 02:20:24 +0800

    integrate get API into assessor table

--- components/table/demo.tsx
+++ components/table/demo.tsx
@@ -1,5 +1,6 @@
 import Button from "@mui/material/Button";
 import { DataGrid, GridColDef, GridApi, GridCellValue } from "@mui/x-data-grid";
+import { useState, useEffect } from 'react';
 
 const columns: GridColDef[] = [
   {
@@ -28,41 +29,29 @@ const columns: GridColDef[] = [
     },
   },
   { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "Website", width: 300 },
-  { field: "lastName", headerName: "Company", width: 300 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: any) =>
-      `${params.getValue(params.id, "firstName") || ""} ${
-        params.getValue(params.id, "lastName") || ""
-      }`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { field: "url", headerName: "Website", width: 300 },
+  { field: "companyName", headerName: "Company", width: 800 },
 ];
 
 export default function DataGridDemo() {
+
+  const [rows, setRows] = useState(() => []);
+
+  useEffect(() => {
+      fetch(
+        "https://asia-southeast1-starlit-array-328711.cloudfunctions.net/hack4good/api/assessments/visa",
+        { mode: "cors" }
+      ).then(response => {
+        return response.json();
+      }).then(data => {
+        JSON.stringify(data);
+        console.log(data);
+        setRows(() => data);
+      })  
+  }, []);
+
   return (
+
     <div
       style={{
         height: 400,
--- src/pages/temp/index.tsx
+++ src/pages/temp/index.tsx
@@ -1,5 +1,5 @@
 import TagBox from "components/TagBox/TagBox";
 
 export default function temp() {
-  return <TagBox stage={1} />;
+  return <TagBox stage={1} setResult={undefined} resultArr={[]} forward={undefined} back={undefined} />;
 }
